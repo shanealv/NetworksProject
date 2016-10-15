f2897179908c5a8a08e8ba7fcd8818a3ad3f065e
shanealv <shane.alv@gmail.com>
2016-10-15 11:23:03 -0600

    Updated FileService

--- Shared/FileService.cpp
+++ Shared/FileService.cpp
@@ -1,10 +1,58 @@
 #include <iostream>
 #include <fstream>
+#include <limits>
+#include <>
 
 using namespace std;
 
-byte[] ReadFile(char * filename, int start, int stop)
+streamsize filesize(const char* filename)
 {
-	byte buffer[BUFFER_SIZE];
-	fstream file()
+	ifstream file;
+	file.open(filename, ios::in | ios::binary);
+	file.ignore(numeric_limits<streamsize>::max());
+	streamsize length = file.gcount();
+	file.clear();
+	file.seekg(0, ios_base::beg);
+	return length;
+}
+
+unsigned char[] ReadFile(const char * filename, int start, int stop)
+{
+	unsigned char buffer[] = new unsigned char[BUFFER_SIZE];
+	int diff = end - start;
+	if (diff > BUFFER_SIZE)
+	{
+		cerr << "Buffer isn't large enough to make read from bytes " << start << " to " << stop << endl;
+		return NULL;
+	}
+	ifstream inf(filename);
+	if (!inf)
+	{
+		cerr << "Could not open " << filename << endl;
+		return NULL;
+	}
+
+	inf.seekg(start, ios::beg);
+	inf.read(&buffer, diff);
+	return buffer;
+}
+
+unsigned char[] WriteFile(const char * filename, int start, int stop, unsigned char buffer[])
+{
+	int diff = end - start;
+	if (diff > BUFFER_SIZE)
+	{
+		cerr << "Buffer isn't large enough to make read from bytes " << start << " to " << stop << endl;
+		return NULL;
+	}
+	ifstream inf(filename);
+	if (!inf)
+	{
+		cerr << "Could not open " << filename << endl;
+		return NULL;
+	}
+
+	inf.seekg(start, ios::beg);
+	inf.write(&buffer, diff);
+	return buffer;
 }
--- Shared/FileService.h
+++ Shared/FileService.h
@@ -2,5 +2,8 @@
 #define FileService_h
 #define	BUFFER_SIZE = 1024
 
+streamsize filesize(const char* filename);
+unsigned char[] ReadFile(const char * filename, int start, int stop);
+unsigned char[] WriteFile(const char * filename, int start, int stop, unsigned char buffer[]);
 
 #endif // !FileService_h
