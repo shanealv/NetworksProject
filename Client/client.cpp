becc39c284e4655cfc025a090ff2e4d285240887
Ben Churchill <bencbchurchill@gmail.com>
2016-10-22 11:07:31 -0600

    got a simple client and server

--- Client/client.cpp
+++ Client/client.cpp
@@ -1,28 +1,57 @@
-#include "client.h"
-#include <err.h>
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
 
-using namespace std;
+void error(const char *msg)
+{
+    perror(msg);
+    exit(0);
+}
 
-//*************** Main Method ***************//
 int main(int argc, char *argv[])
 {
-	//Check for input
-	if (argc != 4)
-	{
-		fprintf(stderr,"Neccessary arguments: IP address, port, file name\n");
-		exit(0);
-	}
-	
-	//** Initialize variables used for socket communication **//
-	//IP Address
-	if (!inet_aton(argv[1], &ip))
-        errx(1, "can't parse IP address %s", ipstr);
-	if ((Server = gethostbyaddr((const void *)&ip, sizeof ip, AF_INET)) == NULL)
-        errx(1, "no name associated with %s", ipstr);
-	
-	
-	//Port Number
-	PortNum = atoi(argv[2]);
-	
+    int sockfd, portno, n;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+
+    char buffer[256];
+    if (argc < 3) {
+       fprintf(stderr,"usage %s hostname port\n", argv[0]);
+       exit(0);
+    }
+    portno = atoi(argv[2]);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) 
+        error("ERROR opening socket");
+    server = gethostbyname(argv[1]);
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, 
+         (char *)&serv_addr.sin_addr.s_addr,
+         server->h_length);
+    serv_addr.sin_port = htons(portno);
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+        error("ERROR connecting");
+    printf("Please enter the message: ");
+    bzero(buffer,256);
+    fgets(buffer,255,stdin);
+    n = write(sockfd,buffer,strlen(buffer));
+    if (n < 0) 
+         error("ERROR writing to socket");
+    bzero(buffer,256);
+    n = read(sockfd,buffer,255);
+    if (n < 0) 
+         error("ERROR reading from socket");
+    printf("%s\n",buffer);
+    close(sockfd);
+    return 0;
 }
